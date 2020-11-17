d8c57dd02889cde88b88ad4c50ff03d7c9d32d63
chinnamlakshmibhavani <46337222+chinnamlakshmibhavani@users.noreply.github.com>
2020-11-17 13:08:30 +0530

    added code for visitor design pattern in java

---
+++ visitor pattern/src/com/designpatterns/ComputerPart.java
@@ -0,0 +1,4 @@
+package com.designpatterns;
+public interface ComputerPart {
+	   public void accept(ComputerPartVisitor computerPartVisitor);
+	}
---
+++ visitor pattern/src/com/designpatterns/Monitor.java
@@ -0,0 +1,8 @@
+package com.designpatterns;
+public class Monitor implements ComputerPart {
+
+	   @Override
+	   public void accept(ComputerPartVisitor computerPartVisitor) {
+	      computerPartVisitor.visit(this);
+	   }
+	}
